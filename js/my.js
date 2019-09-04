50b0b05105fd586014d5d083413f37be0bb87f3c
170806104 <52302341+170806104@users.noreply.github.com>
2019-09-04 13:36:24 +0800

    Add files via upload

--- js/my.js
+++ js/my.js
@@ -1,36 +1,58 @@
-	 
-		function first()
+function btn2()
+		        {
+		    document.getElementById("mydiv").style.display='none';
+		        }
+				
+function btn4()
+		        {
+		    document.getElementById("mydiv2").style.display='none';
+		        }  
+
+
+
+function first()
 		{
-			
-            if (document.getElementsByName('jijie')[2].checked==true)	
-			 alert("做对了，你真厉害！");
-		else
-			alert("错了哦，再好好想一想！");
-			
+			if (document.getElementsByName('jijie')[2].checked==true)	
+			document.getElementById("mydiv").style.display='block';
+		    else
+			document.getElementById("mydiv2").style.display='block';
 		}
+		
+		
 		
 	
 		 function second()
 		 {
 		  if (document.getElementsByName('jieri')[1].checked==true)	
-		 	 alert("做对了，你真厉害！");
+		 	  document.getElementById("mydiv").style.display='block';
 		 else
-		 	alert("错了哦，再好好想一想！");
+		 	 document.getElementById("mydiv2").style.display='block';
 		 	
 		 }
+	  
+		
+		  
 		  function third()
 		 {
 		  if (document.getElementsByName('didian')[0].checked==true)	
-		 	 alert("做对了，你真厉害！");
+		 	 document.getElementById("mydiv").style.display='block';
 		 else
-		 	alert("错了哦，再好好想一想！");
+		     document.getElementById("mydiv2").style.display='block';
 		 	
 		 }
+		 
+		 
+		 
 		   function fourth()
 		 {
 		  if (document.getElementsByName('names')[3].checked==true)	
-		 	 alert("做对了，你真厉害！");
+		 	   document.getElementById("mydiv").style.display='block';
 		 else
-		 	alert("错了哦，再好好想一想！");
+		 	document.getElementById("mydiv2").style.display='block';
 		 	
 		 }
+		  
+
+
+
+	
--- js/my1.js
+++ js/my1.js
@@ -15,17 +15,22 @@
 	  return;}
 	else{ 
 		   if(checkVal=='B,C,') 
-		       {alert("你的答案是："+checkVal+"恭喜你，回答正确！");
-		       return;}
+		       // {alert("你的答案是："+checkVal+"恭喜你，回答正确！");
+			   document.getElementById("mydiv").style.display='block';
+		       // return;}
 			else{
-				alert("你的答案是："+checkVal+"回答错误！");
-				 return;
+// 				alert("你的答案是："+checkVal+"回答错误！");
+// 				 return;
+document.getElementById("mydiv2").style.display='block';
 			}
            
 	
   } 
    } 
- 
+ 		function btn2(){
+     document.getElementById("mydiv").style.display='none';
+ }
+ 	
   function queding()
  {
  var str=document.getElementsByName("answer");
@@ -43,13 +48,22 @@
  	  return;}
  	else{ 
  		   if(checkVal=='B,C,D,') 
- 		       {alert("你的答案是："+checkVal+"恭喜你，回答正确！");
- 		       return;}
+ 		        document.getElementById("mydiv").style.display='block';
+				// {alert("你的答案是："+checkVal+"恭喜你，回答正确！");
+ 		       // return;}
  			else{
- 				alert("你的答案是："+checkVal+"回答错误！");
- 				 return;
+//  				alert("你的答案是："+checkVal+"回答错误！");
+//  				 return;
+document.getElementById("mydiv2").style.display='block';
  			}
            
  	
   } 
    } 
+
+   				
+   function btn4()
+   		        {
+   		    document.getElementById("mydiv2").style.display='none';
+   		        }  
+   
